1bd0bac42f7ed99d839841f62e1aed529d9c91e1
shymelon <gr.gorlov@yandex.ru>
2024-11-24 23:39:17 +0100

    separate login form into component

---
+++ components/LoginForm.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const $router = useRouter();
+const onSubmit = () => {
+  $router.push("/dashboard");
+};
+</script>
+
+<template>
+  <form class="space-y-6" @submit.prevent="onSubmit">
+    <!-- Email Input -->
+    <div>
+      <label for="email" class="sr-only">Почта</label>
+      <input
+        id="email"
+        type="email"
+        class="block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        placeholder="Почта"
+      />
+    </div>
+
+    <!-- Password Input -->
+    <div class="relative">
+      <label for="password" class="sr-only">Пароль</label>
+      <input
+        id="password"
+        type="password"
+        class="block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        placeholder="Пароль"
+      />
+    </div>
+
+    <!-- Register Link -->
+    <a href="#" class="text-indigo-600 hover:underline text-sm"
+      >Зарегистрироваться</a
+    >
+
+    <!-- Submit Button -->
+    <div class="flex mx-auto">
+      <button
+        type="submit"
+        class="mx-auto py-3 font-semibold px-4 inline-flex items-center gap-x-2 text-sm rounded-lg border border-indigo-600 text-indigo-600 hover:border-indigo-500 hover:text-indigo-500 focus:outline-none focus:border-indigo-500 focus:text-indigo-500 disabled:opacity-50 disabled:pointer-events-none"
+      >
+        Войти
+      </button>
+    </div>
+  </form>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,49 +8,14 @@
         <h1 class="text-gray-900 text-4xl font-semibold mb-4 text-center">
           Вход (dev ветка)
         </h1>
-        <form class="space-y-6">
-          <!-- Email Input -->
-          <div>
-            <label for="email" class="sr-only">Почта</label>
-            <input
-              id="email"
-              type="email"
-              class="block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-              placeholder="Почта"
-            />
-          </div>
-
-          <!-- Password Input -->
-          <div class="relative">
-            <label for="password" class="sr-only">Пароль</label>
-            <input
-              id="password"
-              type="password"
-              class="block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-              placeholder="Пароль"
-            />
-          </div>
-
-          <!-- Register Link -->
-          <a href="#" class="text-indigo-600 hover:underline text-sm"
-            >Зарегистрироваться</a
-          >
-
-          <!-- Submit Button -->
-          <div class="flex mx-auto">
-            <button
-              type="submit"
-              class="mx-auto py-3 font-semibold px-4 inline-flex items-center gap-x-2 text-sm rounded-lg border border-indigo-600 text-indigo-600 hover:border-indigo-500 hover:text-indigo-500 focus:outline-none focus:border-indigo-500 focus:text-indigo-500 disabled:opacity-50 disabled:pointer-events-none"
-            >
-              Войти
-            </button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginForm from "~/components/LoginForm.vue";
+</script>
 
 <style scoped></style>

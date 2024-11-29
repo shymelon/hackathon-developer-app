982597fabdf6eec5b85895ad6e8360f082ad0137
Tuan Anh <tp.anhh7@gmail.com>
2024-11-29 12:57:21 +0300

    change projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -11,92 +11,93 @@
         </button>
       </div>
 
-      <ul class="flex gap-x-5 gap-y-10 items-center flex-wrap mt-10">
-        <li
+      <div
+        class="flex gap-x-5 gap-y-10 items-center justify-between flex-wrap mt-10"
+      >
+        <article
           class="w-[32.354%] min-h-[362px] flex items-end border border-solid border-[#E5E7EB] rounded-lg bg-[url('/images/project_bg.png')] bg-no-repeat bg-top-center bg-contain"
         >
-          <NuxtLink
-            to="/projects/project"
-            class="h-fit p-5 bg-white rounded-b-lg"
-          >
-            <h2 class="text-lg text-[#1F2937] font-bold">Проект 1</h2>
-            <p class="text-base text-[#6B7280] font-medium my-4">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <p class="text-sm text-[#6B7280] font-semibold">
-              Last updated 5 mins ago
-            </p>
+          <NuxtLink to="/projects/project">
+            <div class="h-fit p-5 bg-white rounded-b-lg">
+              <h2 class="text-lg text-[#1F2937] font-bold">Проект 1</h2>
+              <p class="text-base text-[#6B7280] font-medium my-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <p class="text-sm text-[#6B7280] font-semibold">
+                Last updated 5 mins ago
+              </p>
+            </div>
           </NuxtLink>
-        </li>
-        <li
+        </article>
+
+        <article
           class="w-[32.354%] min-h-[362px] flex items-end border border-solid border-[#E5E7EB] rounded-lg bg-[url('/images/project_bg.png')] bg-no-repeat bg-top-center bg-contain"
         >
-          <NuxtLink
-            to="/projects/project"
-            class="h-fit p-5 bg-white rounded-b-lg"
-          >
-            <h2 class="text-lg text-[#1F2937] font-bold">Проект 1</h2>
-            <p class="text-base text-[#6B7280] font-medium my-4">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <p class="text-sm text-[#6B7280] font-semibold">
-              Last updated 5 mins ago
-            </p>
+          <NuxtLink to="/projects/project">
+            <div class="h-fit p-5 bg-white rounded-b-lg">
+              <h2 class="text-lg text-[#1F2937] font-bold">Проект 2</h2>
+              <p class="text-base text-[#6B7280] font-medium my-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <p class="text-sm text-[#6B7280] font-semibold">
+                Last updated 5 mins ago
+              </p>
+            </div>
           </NuxtLink>
-        </li>
-        <li
+        </article>
+
+        <article
           class="w-[32.354%] min-h-[362px] flex items-end border border-solid border-[#E5E7EB] rounded-lg bg-[url('/images/project_bg.png')] bg-no-repeat bg-top-center bg-contain"
         >
-          <NuxtLink
-            to="/projects/project"
-            class="h-fit p-5 bg-white rounded-b-lg"
-          >
-            <h2 class="text-lg text-[#1F2937] font-bold">Проект 1</h2>
-            <p class="text-base text-[#6B7280] font-medium my-4">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <p class="text-sm text-[#6B7280] font-semibold">
-              Last updated 5 mins ago
-            </p>
+          <NuxtLink to="/projects/project">
+            <div class="h-fit p-5 bg-white rounded-b-lg">
+              <h2 class="text-lg text-[#1F2937] font-bold">Проект 3</h2>
+              <p class="text-base text-[#6B7280] font-medium my-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <p class="text-sm text-[#6B7280] font-semibold">
+                Last updated 5 mins ago
+              </p>
+            </div>
           </NuxtLink>
-        </li>
-        <li
+        </article>
+
+        <article
           class="w-[32.354%] min-h-[362px] flex items-end border border-solid border-[#E5E7EB] rounded-lg bg-[url('/images/project_bg.png')] bg-no-repeat bg-top-center bg-contain"
         >
-          <NuxtLink
-            to="/projects/project"
-            class="h-fit p-5 bg-white rounded-b-lg"
-          >
-            <h2 class="text-lg text-[#1F2937] font-bold">Проект 1</h2>
-            <p class="text-base text-[#6B7280] font-medium my-4">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <p class="text-sm text-[#6B7280] font-semibold">
-              Last updated 5 mins ago
-            </p>
+          <NuxtLink to="/projects/project">
+            <div class="h-fit p-5 bg-white rounded-b-lg">
+              <h2 class="text-lg text-[#1F2937] font-bold">Проект 4</h2>
+              <p class="text-base text-[#6B7280] font-medium my-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <p class="text-sm text-[#6B7280] font-semibold">
+                Last updated 5 mins ago
+              </p>
+            </div>
           </NuxtLink>
-        </li>
-      </ul>
+        </article>
+      </div>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
 // const App = {
-// 	data() {
-// 		return {
-// 			projectNumber: 0,
-// 			projects: [],
-// 		};
-// 	},
+//   data() {
+//     return {
+//       projectNumber: 0,
+//       projects: [],
+//     };
+//   },
 
-// 	methods: {},
+//   methods: {},
 // };
-// createApp(App).mount('#app');
+// createApp(App).mount("#app");
 </script>
 
 <style scoped></style>
